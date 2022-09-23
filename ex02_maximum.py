a9a91f2cfae4847fef9299cf1592580ae70e57cd
tenomiss <64487131+tenomiss@users.noreply.github.com>
2022-09-23 08:13:06 +0200

    Update ex02_maximum.py

--- ex02_maximum.py
+++ ex02_maximum.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def maximum(liste):
-  m = 0
+  m = liste[0]
   for e in liste:
     if e>m:
       m = e
@@ -9,10 +9,12 @@ def maximum(liste):
 
 def test_maximum():
   print('Test de la fonction maximum')
+  assert maximum([-1, -3, -20]) == -1
   assert maximum([10])==10
   assert maximum([15,2,6,0])==15
   assert maximum([8,9,3,12])==12
   assert maximum([1,7,13,4])==13
+  
   print('  OK')
 
 test_maximum()
