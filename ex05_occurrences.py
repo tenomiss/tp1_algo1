74ef43292ad7d56f1fa4258e09fc873dcca66a2a
tenomiss <64487131+tenomiss@users.noreply.github.com>
2022-09-23 09:41:19 +0200

    Update ex05_occurrences.py

--- ex05_occurrences.py
+++ ex05_occurrences.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from tkinter import N
+
+
 def nb_occurrences(liste,valeur):
   nb = 0
   for e in liste:
@@ -35,3 +38,12 @@ def test_nb_occurrences():
   print('  OK')
 
 test_nb_occurrences()
+assert nb_occurrences([],0) == 0
+assert nb_occurrences([1],1) == 1
+assert nb_occurrences([5],3) == 0
+assert nb_occurrences([5, 5, 6, 7, 8, 9], 1) == 0
+assert nb_occurrences([5,6,7,8,9], 5) == 1
+assert nb_occurrences([5,6,7,8,9],9) == 1
+assert nb_occurrences([5,6,7,8,9], 7) == 1 
+assert nb_occurrences([5,6,6,7,8,9],6) == 2
+assert nb_occurrences([5,5,5,5,5,5,5], 5) == 7
